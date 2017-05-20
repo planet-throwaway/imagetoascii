bc95c6c0eedf7d96019a3fea8250342ffd6885d3
Patrick Armstrong <pat@oldpatricka.com>
2017-05-19 21:23:16 -0700

    Initial commit. Working ASCII conversion.

---
+++ Makefile
@@ -0,0 +1,32 @@
+SOURCEDIR=.
+SOURCES := $(shell find $(SOURCEDIR) -name '*.go')
+TESTABLE_PACKAGES := $(shell go list ./...)
+TIMESTAMP := $(shell date +%s)
+
+VERSION=0.1.0
+
+LDFLAGS=-ldflags '-X main.version=${VERSION}'
+
+BUILDDIR=build
+
+BINARY=imagetoascii
+DOCKER_BINARY=$(BINARY).docker
+
+.DEFAULT_GOAL: $(BUILDDIR)/$(BINARY)
+
+$(BUILDDIR)/$(BINARY): ${SOURCES}
+	go build ${LDFLAGS} -o build/${BINARY} *.go
+
+$(BUILDDIR)/$(BINARY).docker: ${SOURCES}
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build ${LDFLAGS} -o build/${DOCKER_BINARY} *.go
+
+docker: $(BUILDDIR)/$(DOCKER_BINARY)
+	docker build -t ${BINARY}:${VERSION} -t ${BINARY} .
+
+.PHONY: test
+test:
+	go test -v ${TESTABLE_PACKAGES}
+
+.PHONY: clean
+clean:
+	@if [ -f "${BUILDDIR}/${BINARY}" ] ; then rm "${BUILDDIR}/${BINARY}" ; fi
---
+++ convert.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	base64 "encoding/base64"
+	"log"
+	"strings"
+
+	resize "github.com/nfnt/resize"
+	"image"
+	color "image/color"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+)
+
+var (
+	// Reference: http://paulbourke.net/dataformats/asciiart/
+	asciiMap     = strings.Split("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ", "")
+	maxGrayColor = 256
+	// Factor to convert 256 color bit grayscale to 70 "color" ascii
+	grayscaleToAsciiMultiplier      = 1.0 / float64(maxGrayColor) * float64(len(asciiMap)-1)
+	maxImageWidth              uint = 80
+)
+
+func Convert(imageBase64 string) string {
+	log.Print("Converting Image")
+	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(imageBase64))
+
+	m, _, err := image.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resizedImage := resizeForAscii(m)
+	bounds := resizedImage.Bounds()
+
+	output := ""
+
+	// Convert image to grayscale
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			colorPixel := resizedImage.At(x, y)
+			grayAscii := pixelColorToAscii(colorPixel)
+			output = output + grayAscii
+		}
+		output = output + "\n"
+	}
+	return output
+}
+
+func resizeForAscii(img image.Image) image.Image {
+
+	bounds := img.Bounds()
+
+	newWidth := uint(bounds.Max.X)
+	newHeight := uint(bounds.Max.Y / 2)
+
+	if newWidth > maxImageWidth {
+		ratio := newWidth / newHeight
+		newWidth = maxImageWidth
+		newHeight = maxImageWidth / ratio
+	}
+
+	return resize.Resize(newWidth, newHeight, img, resize.NearestNeighbor)
+}
+
+func pixelColorToAscii(colorPixel color.Color) string {
+	grayPixel := color.GrayModel.Convert(colorPixel)
+	grayColor := grayPixel.(color.Gray)
+	grayAscii := grayToAsciiIndex(grayColor.Y)
+	return asciiMap[grayAscii]
+}
+
+func grayToAsciiIndex(gray uint8) int {
+	asciiFloat := float64(gray) * grayscaleToAsciiMultiplier
+	ascii := int(asciiFloat)
+	return ascii
+}
